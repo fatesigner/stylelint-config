c366c13add99ea1221862a8ee8ec9a75a8957c80
fatesigner <fatesigner@gmail.com>
2019-08-20 15:31:42 +0800

    initial: initial commit.

---
+++ build/env.js
@@ -0,0 +1,18 @@
+/**
+ * global
+ */
+
+const Path = require('path');
+
+module.exports = function () {
+  const rootPath = Path.resolve(__dirname, '..');
+  const buildPath = Path.join(rootPath, 'build');
+  const srcPath = Path.join(rootPath, 'src');
+  const outputPath = Path.join(rootPath, 'dist');
+  return {
+    rootPath,
+    buildPath,
+    srcPath,
+    outputPath
+  };
+};
---
+++ src/index.js
@@ -0,0 +1,28 @@
+﻿/**
+ * stylelint.config
+ */
+
+module.exports = {
+  extends: ['stylelint-config-standard', 'stylelint-config-recess-order', 'stylelint-config-css-modules'],
+  plugins: [
+    'stylelint-order'
+  ],
+  rules: {
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: ['mixin', 'extend', 'content', 'function', 'if', 'each', 'include']
+      }
+    ],
+    indentation: 2,
+    'selector-pseudo-element-no-unknown': 0,
+    'selector-pseudo-class-case': 'lower',
+    'selector-pseudo-class-no-unknown': [
+      true,
+      {
+        ignorePseudoClasses: ['global']
+      }
+    ]
+  }
+};
+
