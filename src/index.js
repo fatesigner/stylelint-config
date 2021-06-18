591218cc95741d7ae9111e33cd109ecb83f1d6b5
fatesigner <fatesigner@gmail.com>
2021-06-18 18:05:59 +0800

    feat: add rules

--- src/index.js
+++ src/index.js
@@ -6,19 +6,19 @@ module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-config-recess-order', 'stylelint-config-css-modules'],
   plugins: ['stylelint-order'],
   rules: {
+    indentation: 2,
     'at-rule-no-unknown': [
       true,
-      {
-        ignoreAtRules: ['mixin', 'extend', 'content', 'function', 'if', 'each', 'include']
-      }
+      { ignoreAtRules: ['tailwind', 'mixin', 'extend', 'content', 'function', 'if', 'each', 'include'] }
     ],
-    indentation: 2,
+    'declaration-block-trailing-semicolon': null,
+    'declaration-colon-newline-after': null,
+    'property-no-unknown': [false, { ignoreProperties: [] }],
+    'selector-pseudo-element-no-unknown': [true, { ignorePseudoElements: ['ng-deep', 'v-deep'] }],
     'selector-pseudo-class-case': 'lower',
-    'selector-pseudo-class-no-unknown': [
-      true,
-      {
-        ignorePseudoClasses: ['global']
-      }
-    ]
+    'selector-pseudo-class-no-unknown': [true, { ignorePseudoClasses: ['deep', 'global'] }],
+    'selector-type-no-unknown': [true, { ignoreTypes: [/^uni-/] }],
+    'unit-no-unknown': [true, { ignoreUnits: ['rpx'] }],
+    'value-list-comma-newline-after': null
   }
 };
